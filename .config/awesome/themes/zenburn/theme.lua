3c9c87c88c633e0526961a2e9387e08bfe9cfa54
Pavel Ruban <pruban@adyax.com>
2014-07-09 01:16:01 +0700

    changes

--- .config/awesome/themes/zenburn/theme.lua
+++ .config/awesome/themes/zenburn/theme.lua
@@ -9,7 +9,7 @@
 -- {{{ Main
 theme = {}
 --theme.wallpaper = "/root/linux.jpg"
-theme.wallpaper = "/root/.config/awesome/themes/blame.jpg"
+theme.wallpaper = "/root/.config/awesome/themes/zenburn/blame2.jpg"
 -- }}}
 
 -- {{{ Styles
