bdb4acd214c20a68a1034bef8fabfed316a691e5
Pavel Ruban <job.pavelruban@gmail.com>
2020-05-29 16:16:46 +0300

    Adjust install script.

--- shared/Ubuntu/install_zsh.sh
+++ shared/Ubuntu/install_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install zsh zsh-common zsh-syntax-highlighting
+sudo apt -y install zsh zsh-common zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 cd ~ || exit
@@ -9,12 +9,16 @@ if [ ! -r ~/.zshrc ]
 then
   wget https://raw.githubusercontent.com/pavel-ruban/Linux-Configs/master/shared/Ubuntu/.zshrc
 else
-  echo '!warning: you already have ~/.zshrc file, skipping this file downloading.'
+  echo '!warning: you already have ~/.zshrc file, moved it to ~/.zshrc.old'
+  mv ~/.zshrc ~/.zshrc.old
+  wget https://raw.githubusercontent.com/pavel-ruban/Linux-Configs/master/shared/Ubuntu/.zshrc
 fi
 
 if [ ! -r ~/.zsh-theme ]
 then
   wget https://raw.githubusercontent.com/pavel-ruban/Linux-Configs/master/shared/Ubuntu/.zsh-theme
 else
-  echo '!warning: you already have ~/.zsh-theme skipping this file downloading.'
+  echo '!warning: you already have ~/.zsh-theme file, moved it to ~/.zsh-theme.old'
+  mv ~/.zsh-theme ~/.zsh-theme.old
+  wget https://raw.githubusercontent.com/pavel-ruban/Linux-Configs/master/shared/Ubuntu/.zsh-theme
 fi
