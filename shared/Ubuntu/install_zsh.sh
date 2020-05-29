2c4864ad55b904c87d80c0275966dd054feac38b
Pavel Ruban <job.pavelruban@gmail.com>
2020-05-29 18:33:23 +0300

    change default sh

--- shared/Ubuntu/install_zsh.sh
+++ shared/Ubuntu/install_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt -y install zsh zsh-common
+sudo apt -y install zsh zsh-common expect
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
