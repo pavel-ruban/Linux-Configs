7159ae5c62baa10cac2a999f5ff9cd2ccee2e512
Pavel Ruban <job.pavelruban@gmail.com>
2020-05-29 16:35:55 +0300

    adjust install script

--- shared/Ubuntu/install_zsh.sh
+++ shared/Ubuntu/install_zsh.sh
@@ -3,7 +3,7 @@
 sudo apt -y install zsh zsh-common
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cd ~ || exit
 
 if [ ! -r ~/.zshrc ]
